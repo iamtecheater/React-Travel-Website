a3ae62fa0dc9e7109414a417c32451dd50bcc773
sh1karasu <bthuntervargr@gmail.com>
2021-04-26 16:12:03 +0200

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import '../../css/App.css';
+import HeroSection from "../HeroSection";
+import Cards from '../Cards';
+import Footer from "../Footer";
+
+function Home() {
+    return(
+        <>
+        <HeroSection />
+        <Cards />
+        <Footer />
+        </>
+    )
+}
+
+export default Home;
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../css/App.css";
+import Footer from "../Footer";
+
+export default function SignUp(){
+    return(
+        <>
+        <h1 className="sign-up">SIGN UP</h1>
+      
+        <Footer />
+    </>
+    ) 
+   
+}
